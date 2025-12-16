16ef3262fe4bdb020ecbdaa75144e6785065853e
MaksimNorvatkin <norvatkin_maksim@mail.ru>
2025-12-16 20:43:59 +0300

    Initial commit

---
+++ src/main/java/ru/top/dz_20responseentity/Dz20responseentityApplication.java
@@ -0,0 +1,13 @@
+package ru.top.dz_20responseentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz20responseentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dz20responseentityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/top/dz_20responseentity/Dz20responseentityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.top.dz_20responseentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz20responseentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
